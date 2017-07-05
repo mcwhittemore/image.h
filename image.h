50d5b79f2ab7ae011e520cc26ad925ac9e635b15
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-07-05 14:58:40 -0400

    almost have the base work

--- image.h
+++ image.h
@@ -1,150 +1,60 @@
 #pragma once
 
 #include "./exceptions.h"
-
-using namespace std; 
+#include <vector>
 
 namespace pixicog {
 
 class Image {
   private:
-    bool setup;
-    bool copy;
-    bool debug;
-    unsigned char* pix;
-    int ToPos(int x, int y, int c);
-    int size;
+    std::vector<unsigned char> image;
     int width;
     int height;
     int numChannels;
+    int toPos(int x, int y, int c);
 
   public:
-    Image(bool d = true);
-    Image(const Image &c) : pix(c.pix), setup(c.setup), copy(true) {}; // copy constuctor
+    Image(int w, int h, int nc);
+    Image(const Image &c);
     ~Image();
-    void Setup(int w, int h, int c=3);
-    void Open(string file);
-    void Paint(unsigned char* color);
-    int Size() { return size; };
-    int Width() { return width; };
-    int Height() { return height; };
-    int NumChannels() { return numChannels; };
-    unsigned char Get(int x, int y, int c);
-    unsigned char Get(int p);
-    void Set(int x, int y, int c, int v);
-    void Set(int p, int v);
-    void Set(int x, int y, unsigned char* color);
-    void Save(string file);
-    static void Init(char *argv) {
-      //Magick::InitializeMagick(argv);
-    };
+    unsigned char get(int x, int y, int c);
+    void set(int x, int y, int c, unsigned char v);
 };
 
 /** Constructur **/
 
-Image::Image(bool d) : debug(d), setup(false), pix(nullptr), height(0), width(0), size(0), numChannels(0), copy(false) {
-  if (debug) {
-    cout << this << " create image\n";
-  }
-}
+Image::Image(int w, int h, int nc) : height(h), width(w), numChannels(nc), image(w*h*nc) {}
+
+// copy constuctor
+Image::Image(const Image &c) : height(c.height), width(c.width), numChannels(c.numChannels), image(c.image) {}
 
 /** Deconstructor **/
 
 Image::~Image() {
-  if (setup == false || copy == true) return; 
-  setup = false;
-  free(pix);
-  if (debug) {
-    cout << this << " delete image\n";
-  }
+  // do I need to kill image?
 }
 
 /** PRIVATE - turn x, y, c into an pos in the underlaying array **/
 
-int Image::ToPos(int x, int y, int c) {
+int Image::toPos(int x, int y, int c) {
   return (y*width*numChannels)+(x*numChannels) + c;
 }
 
-/** Config the under laying underlaying **/
+/** PUBLIC **/
 
-void Image::Setup(int w, int h, int c) {
-  if (setup) throw ImageMultipleSetup;
-  setup = true;
-  width = w;
-  height = h;
-  numChannels = c;
-  size = width * height * numChannels;
-  pix = static_cast<unsigned char*>(malloc(size));
-}
-
-void Image::Open(string path) {
-  if (setup) throw ImageMultipleSetup;
-  /*Magick::Image image; 
-  image.read(path);
-
-  width = (int) image.columns();
-  height = (int) image.rows();
-  numChannels = (int) image.channels();
-  Setup(width, height, numChannels);
-
-  MagickCore::Quantum *pixels = image.getPixels(0, 0, width, height);
-
-  for (int x=0; x<width; x++) {
-    for (int y=0; y<height; y++) {
-      unsigned offset = numChannels * (width * y + x);
-      for (int c=0; c<numChannels; c++) {
-        pix[offset+c] = pixels[offset+c];
-      }
-    }
-  }
-  */
-  
-}
-
-unsigned char Image::Get(int p) {
-  if (!setup) throw ImageNotSetup;
-  return pix[p];
-}
-
-unsigned char Image::Get(int x, int y, int c) {
-  if (!setup) throw ImageNotSetup;
-  int pos = ToPos(x, y, c);
-  return pix[pos];
+unsigned char Image::get(int x, int y, int c) {
+  int pos = toPos(x, y, c);
+  return image[pos];
 }
 
 // Set a specific channel for one pixel
-void Image::Set(int x, int y, int c, int v) {
-  if (!setup) throw ImageNotSetup;
-  int pos = ToPos(x, y, c);
-  pix[pos] = v;
-}
-
-// Set all channels for one pixel
-void Image::Set(int x, int y, unsigned char* color) {
-  if (!setup) throw ImageNotSetup;
- int pos = ToPos(x, y, 0); 
- for (int c=0; c<numChannels; c++) {
-   pix[pos+c] = color[c];
- }
-}
-
-//Set a specific value in the underlaying array
-void Image::Set(int p, int v) {
-  pix[p] = v;
-}
-
-void Image::Save(string file) {
-  if (!setup) throw ImageNotSetup;
-  // Create Image object and read in from pixel data above
-  /*Magick::Image image; 
-  if (debug) {
-    cout << this << " save image\n";
-  }
-  image.read(width, height,"RGB",Magick::CharPixel,pix);
-
-  // Write the image to a file - change extension if you want a GIF or JPEG
-  image.write(file); 
-  */
+void Image::set(int x, int y, int c, unsigned char v) {
+  if (x >= width || x < 0) throw "X coordinate is out of range";
+  if (y >= height || y < 0) throw "Y coordinate is out of range";
+  if (c >= numChannels || c < 0) throw "Color channel is out of range";
+  if (v > 255 || v < 0) throw "Color value is out of range";
+  int pos = toPos(x, y, c);
+  image[pos] = v;
 }
 
 }
--- test.cpp
+++ test.cpp
@@ -7,25 +7,67 @@
 using namespace pixicog;
 
 bool setPixelInRange() {
-  // r
+  Image img(5, 5, 3);
+  // red
+  img.set(0, 0, 0, 255);
+  if (img.get(0, 0, 0) != 255) return false;
   // g
+  img.set(3, 2, 1, 100);
+  if (img.get(3, 2, 1) != 100) return false;
   // b
-  // a
-  return false;
+  img.set(4, 4, 2, 45);
+  if (img.get(4, 4, 2) != 45) return false;
+  return true;
 };
 
 bool setPixelOutOfRange() {
+  // TODO: assert the error messages are valid
+  Image img(4, 4, 4);
+
   // bad x
+  try {
+    img.set(5, 0, 0, 255);
+    return false;
+  }
+  catch (const char* msg) {
+  }
+
   // bad y
+  try {
+    img.set(0, 5, 0, 255);
+    return false;
+  }
+  catch (const char* msg) {
+  }
+
   // bad c
-  return false;
+  try {
+    img.set(0, 0, 5, 255);
+    return false;
+  }
+  catch (const char* msg) {
+  }
+
+  // bad v
+  try {
+    img.set(0, 0, 5, 256);
+    return false;
+  }
+  catch (const char* msg) {
+  }
+
+  
+  return true;
 };
 
 bool createImage() {
   return false;
 };
 
-bool createImageWithArgs() {
+bool copyImge() {
+  Image img(3, 3, 3);
+  Image copy(img);
+
   return false;
 }
 
@@ -33,8 +75,9 @@ bool createImageWithArgs() {
 int main(int argc, char * argv[]) {
   tap::start();
   tap::test("# creating the image object");
-  tap::pass(createImage(), "with no arguments");
-  tap::pass(createImageWithArgs(), "with all arguments");
+  tap::pass(createImage(), "creating should work");
+  tap::pass(badCreateShouldThrow(), "bad create should throw");
+  tap::pass(copyImage(), "copying should work");
 
   tap::test("# setting a pixels");
   tap::pass(setPixelInRange(), "set a pixel in range should work");
