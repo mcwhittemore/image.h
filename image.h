daf79772d8cab55089083d68a210070fc5fb71f6
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2017-07-07 13:54:26 -0400

    starting to write docs for image

--- image.h
+++ image.h
@@ -13,6 +13,7 @@ class Image {
     int toPos(int x, int y, int c);
 
   public:
+    Image();
     Image(int w, int h, int nc);
     Image(const Image &c);
     ~Image();
@@ -21,11 +22,10 @@ class Image {
 };
 
 /** Constructur **/
+Image::Image() {}
 
 Image::Image(int w, int h, int nc) : height(h), width(w), numChannels(nc), image(w*h*nc) {
-  if (height <= 0) throw "Invalid height value";
-  if (width <= 0) throw "Invalid width value";
-  if (numChannels < 3 || numChannels > 4) throw "Invalid numChannels value";
+  validate();
 }
 
 // copy constuctor
@@ -39,11 +39,37 @@ Image::~Image() {
 
 /** PRIVATE - turn x, y, c into an pos in the underlaying array **/
 
+
+void Image::validate() {
+  if (height <= 0) throw "Invalid height value";
+  if (width <= 0) throw "Invalid width value";
+  if (numChannels !== 1 || numChannels != 3 || numChannels != 4) throw "Invalid numChannels value";
+}
+
 int Image::toPos(int x, int y, int c) {
+  if (x >= width || x < 0) throw "X coordinate is out of range";
+  if (y >= height || y < 0) throw "Y coordinate is out of range";
+  if (c >= numChannels || c < 0) throw "Color channel is out of range";
+
+  // TODO: should we let 0-3 work for numChannels at all times?
   return (y*width*numChannels)+(x*numChannels) + c;
 }
 
 /** PUBLIC **/
+
+/**
+ * @param: w {Int} - the width of the image
+ * @param: h {Integer} - the height of the image
+ * @param: nc {Integer} - the number of channels (1, 3, 4) in the image.
+ * @param: img {std::vector<unsigned char>} - the raw data for the image.
+ */
+void Image::setup(int w, int h, int nc, std::vector<unsigned char> img) {
+  width = w;
+  height = h;
+  numChannels = nc;
+  image = img;
+  validate();
+}
 
 unsigned char Image::get(int x, int y, int c) {
   int pos = toPos(x, y, c);
@@ -51,12 +77,12 @@ unsigned char Image::get(int x, int y, int c) {
 }
 
 // Set a specific channel for one pixel
+/**
+ *
+ */
 void Image::set(int x, int y, int c, unsigned char v) {
-  if (x >= width || x < 0) throw "X coordinate is out of range";
-  if (y >= height || y < 0) throw "Y coordinate is out of range";
-  if (c >= numChannels || c < 0) throw "Color channel is out of range";
-  if (v > 255 || v < 0) throw "Color value is out of range";
   int pos = toPos(x, y, c);
+  if (v > 255 || v < 0) throw "Color value is out of range";
   image[pos] = v;
 }
 
